c48aadd6214588b995c7b4e5ddef768f6d2aa813
ItchyPigeon54 <rjhunt1990@gmail.com>
2026-05-28 19:26:18 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './solo_pixelmon_guild_recruiting_page.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
